e8137cc5c15e29f0ba2fca969ca4b2d9d4637d4b
gustavo@mumuki.org <gustavo@mumuki.org>
2021-10-13 14:43:56 +0000

    Mumuki Export on 2021-10-13 14:43:56 +0000

--- 00007_Ejercicio 7/test.rb
+++ 00007_Ejercicio 7/test.rb
@@ -1,37 +1,37 @@
-it "Si creo un Auto le puedo especificar su nafta inicial" do
-  auto = Auto.new(50)
-  expect(auto.nafta).to be 50
+it "Si creo una instancia de $CLASE le puedo especificar su nafta inicial" do
+  vehiculo = $CLASE.new(50)
+  expect(vehiculo.nafta).to be 50
 end
 
-it "Un Auto suma 20 litros de nafta al enviarle $metodoParaCargar(20)" do
-  auto = Auto.new(50)
-  auto.$metodoParaCargar(20)
-  expect(auto.nafta).to be 70
+it "Una instancia de $CLASE suma 20 litros de nafta al enviarle $METODO_CARGAR(20)" do
+  vehiculo = $CLASE.new(50)
+  vehiculo.$METODO_CARGAR(20)
+  expect(vehiculo.nafta).to be 70
 end
 
-it "Un Auto suma 5 litros de nafta al enviarle $metodoParaCargar(5)" do
-  auto = Auto.new(50)
-  auto.$metodoParaCargar(5)
-  expect(auto.nafta).to be 55
+it "Una instancia de $CLASE suma 5 litros de nafta al enviarle $METODO_CARGAR(5)" do
+  vehiculo = $CLASE.new(50)
+  vehiculo.$METODO_CARGAR(5)
+  expect(vehiculo.nafta).to be 55
 end
 
-it "Un Auto suma 100 litros de nafta al enviarle $metodoParaCargar(100)" do
-  auto = Auto.new(50)
-  auto.$metodoParaCargar(100)
-  expect(auto.nafta).to be 150
+it "Una instancia de $CLASE suma 100 litros de nafta al enviarle $METODO_CARGAR(100)" do
+  vehiculo = $CLASE.new(50)
+  vehiculo.$METODO_CARGAR(100)
+  expect(vehiculo.nafta).to be 150
 end
 
-it "Un Auto no está cargado si tiene $cantidadSuficienteDeCarga litros de nafta" do
-  auto = Auto.new($cantidadSuficienteDeCarga)
-  expect(auto.$metodoParaVerificarNafta).to be false
+it "Una instancia de $CLASE no tiene carga suficiente si tiene $CANTIDAD_CARGA litros de nafta" do
+  vehiculo = $CLASE.new($CANTIDAD_CARGA)
+  expect(vehiculo.$METODO_VERIFICAR).to be false
 end
 
-it "Un Auto no está cargado si tiene menos de $cantidadSuficienteDeCarga litros de nafta" do
-  auto = Auto.new($cantidadSuficienteDeCarga - 1)
-  expect(auto.$metodoParaVerificarNafta).to be false
+it "Una instancia de $CLASE no tiene carga suficiente si tiene menos de $CANTIDAD_CARGA litros de nafta" do
+  vehiculo = $CLASE.new($CANTIDAD_CARGA - 1)
+  expect(vehiculo.$METODO_VERIFICAR).to be false
 end
 
-it "Un Auto está cargado si tiene más de $cantidadSuficienteDeCarga litros de nafta" do
-  auto = Auto.new($cantidadSuficienteDeCarga + 1)
-  expect(auto.$metodoParaVerificarNafta).to be true
+it "Una instancia de $CLASE tiene carga suficiente si tiene más de $CANTIDAD_CARGA litros de nafta" do
+  vehiculo = $CLASE.new($CANTIDAD_CARGA + 1)
+  expect(vehiculo.$METODO_VERIFICAR).to be true
 end
