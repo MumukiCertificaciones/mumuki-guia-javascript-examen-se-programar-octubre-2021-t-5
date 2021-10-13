15fe5366a301fce8b2b9e6dd429c462a816aca85
gustavo@mumuki.org <gustavo@mumuki.org>
2021-10-13 13:51:47 +0000

    Mumuki Export on 2021-10-13 13:51:47 +0000

--- 00003_Ejercicio 3/test..js
+++ 00003_Ejercicio 3/test..js
@@ -1,23 +1,21 @@
 describe("", function() {
-  it("Es correcto que 3 menos 2 es menor a 6", function() {
-    assert($nombreFuncion(3, 2, 6));
+  it("Es falso que 3 menos 2 es mayor a 6", function() {
+    assert(!$FUNCION(3, 2, 6));
   });
   
-  it("Es correcto que 5 menos 4 es menor a 20", function() {
-    assert($nombreFuncion(5, 4, 20));
+  it("Es falso que 5 menos 4 es mayor a 20", function() {
+    assert(!$FUNCION(5, 4, 20));
   });
   
-  it("Es correcto que 18 menos 3 es menor a 18", function() {
-    assert($nombreFuncion(18, 3, 18));
+  it("Es falso que 18 menos 3 es mayor a 18", function() {
+    assert(!$FUNCION(18, 3, 18));
   });
   
-  it("No es correcto que 7 menos 4 es menor a 2", function() {
-    assert(!$nombreFuncion(7, 4, 2));
+  it("Es verdadero que 7 menos 4 es mayor a 2", function() {
+    assert($FUNCION(7, 4, 2));
   });
   
-  it("No es correcto que 9 menos 3 es menor a 5", function() {
-    assert(!$nombreFuncion(9, 3, 5));
+  it("Es verdadero que 9 menos 3 es mayor a 5", function() {
+    assert($FUNCION(9, 3, 5));
   });
-  
-  
 })
